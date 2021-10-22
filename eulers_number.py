ff96a30a4cd3db9aa2c812a1512067b1560ad4c3
Aaron <69485872+chop-code@users.noreply.github.com>
2021-10-22 19:45:40 +0100

    maclaurin series

--- eulers_number.py
+++ eulers_number.py
@@ -7,3 +7,7 @@ def fact(i):
 power_of_e = int(input("Power of e: "))
 iterations = int(input("Number of iterations: "))
 sum = 1
+
+for i in range(1,iterations+1):
+    sum = sum + pow(power_of_e,i)/fact(i)
+    print(f"{sum:.20}")
